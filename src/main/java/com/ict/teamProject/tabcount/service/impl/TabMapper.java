24340e9833257b48e423ed20e5a3a1571f83d2bd
hanmincheol <hmc01100@gmail.com>
2024-02-29 10:33:10 +0900

    페이지 DB저장 완료

---
+++ src/main/java/com/ict/teamProject/tabcount/service/impl/TabMapper.java
@@ -0,0 +1,37 @@
+package com.ict.teamProject.tabcount.service.impl;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Mapper;
+
+
+@Mapper
+public interface TabMapper {
+
+	Integer findComm(LocalDate today);
+
+	void setComm(Integer num);
+
+	void insertComm();
+
+	Integer findExer(LocalDate today);
+
+	void setExer(Integer num);
+
+	void insertExer();
+
+	Integer findDiet(LocalDate today);
+
+	void setDiet(Integer num);
+
+	void insertDiet();
+
+	Integer findDiary(LocalDate today);
+
+	void setDiary(Integer num);
+
+	void insertDiary();
+
+}
